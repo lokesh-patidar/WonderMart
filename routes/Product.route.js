17d301a688e895274c465ab4b1f7bbabbd3989ed
Lokesh_Patidar <bansiyalokesh29988@gmail.com>
2023-01-18 12:51:33 +0530

    wonder mart

--- routes/Product.route.js
+++ routes/Product.route.js
@@ -9,12 +9,25 @@ productRouter.get("/data", async (req, res) => {
     try {
         let products = await ProductModel.find();
         res.send(products);
-    } 
+    }
     catch (err) {
         res.send("Something went wrong!");
         console.log(err);
     }
 });
+
+productRouter.post("/addmany", async (req, res) => {
+    const payload = req.body;
+    try {
+        const product = new ProductModel.insertMany(payload);
+        await product.save();
+        res.send("Products added!");
+        console.log(product);
+    } catch (err) {
+        console.log(err);
+        res.send({ msg: "something went wrong" });
+    }
+})
 
 
 productRouter.use(ValidationForProducts);
@@ -29,7 +42,7 @@ productRouter.post("/add", async (req, res) => {
         console.log(product);
     } catch (err) {
         console.log(err);
-        res.send({ msg: "soething went wrong" });
+        res.send({ msg: "something went wrong" });
     }
 });
 
@@ -41,7 +54,7 @@ productRouter.patch("/update/:id", async (req, res) => {
     try {
         await ProductModel.findByIdAndUpdate({ "_id": id }, payload);
         res.send("product Updated!");
-    } 
+    }
     catch (error) {
         console.log(err);
         res.send({ msg: "soething went wrong" });
@@ -51,7 +64,7 @@ productRouter.patch("/update/:id", async (req, res) => {
 
 productRouter.delete("/delete/:id", async (req, res) => {
     const id = req.params.id;
-    
+
     try {
         await ProductModel.findByIdAndDelete({ "_id": id });
         res.send("Product Deleted!");
