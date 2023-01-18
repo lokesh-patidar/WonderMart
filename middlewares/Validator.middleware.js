486dda79639a9fcbdec587377f7672104a4617f1
Lokesh_Patidar <bansiyalokesh29988@gmail.com>
2023-01-18 13:48:47 +0530

    Wonder Mart

--- middlewares/Validator.middleware.js
+++ middlewares/Validator.middleware.js
@@ -1,6 +1,6 @@
 const Validator = (req, res, next) => {
     if (req.method === "POST") {
-        if (req.url === "/admin/signup" || req.url === "/user/signup") {
+        if (req.url === "users/admin/signup" || req.url === "users/user/signup") {
             if (req.body.username && req.body.email && req.body.password) {
                 next();
             } 
