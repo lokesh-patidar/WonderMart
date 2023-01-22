79a35bbb494ce01f3f5aab1356c2e08955938cc4
Lokesh_Patidar <bansiyalokesh29988@gmail.com>
2023-01-22 15:20:30 +0530

    wm

--- routes/Product.route.js
+++ routes/Product.route.js
@@ -74,11 +74,11 @@ productRouter.get("/q", async (req, res) => {
             res.send(sortedData);
         }
         else if(query.category === 1){
-            const data = await ProductModel.find(query);
+            const data = await ProductModel.find();
             res.send(data);
         }
         else{
-            const data = await ProductModel.find();
+            const data = await ProductModel.find(query);
             res.send(data);
         }
     }
