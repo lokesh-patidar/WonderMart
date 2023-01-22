78cdac677efa85f06727d7a6cd363335a48fab9f
Lokesh_Patidar <bansiyalokesh29988@gmail.com>
2023-01-22 14:46:18 +0530

    Wonder mart

--- routes/Product.route.js
+++ routes/Product.route.js
@@ -73,7 +73,10 @@ productRouter.get("/q", async (req, res) => {
             const sortedData = await ProductModel.find(query).sort({ price: query.sortBy });
             res.send(sortedData);
         }
-        else {
+        else if(query === "wondermart") {
+            const data = await ProductModel.find(query);
+            res.send(data);
+        }else{
             const data = await ProductModel.find(query);
             res.send(data);
         }
